5565660998a19bc23bb9689f7b421a90dcd5d660
Joao Gilberto Magalhaes <joao@byjg.com.br>
2024-11-20 17:57:33 -0600

    Simulate error described in https://github.com/byjg/php-migration/pull/53

---
+++ tests/script-call-test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+export MIGRATE_CONNECTION=sqlite:///tmp/teste.db
+export MIGRATE_PATH=example/sqlite/
+
+echo "Reset and update the database"
+scripts/migrate reset --yes -vvv
+scripts/migrate version
+
+echo "Update the database to version 1"
+scripts/migrate update -vvv --up-to=1
+scripts/migrate version
+
+echo "Update the database to version 2"
+scripts/migrate up -vvv
+scripts/migrate version
+
+echo "Update the database to version 0 (base)"
+scripts/migrate down -vvv --up-to=0
+scripts/migrate version
+
+echo "Update the database to version 0 (base)"
+scripts/migrate up -vvv
+scripts/migrate down -vvv
+scripts/migrate version
+
+echo "Update the database to version 1"
+scripts/migrate update -vvv --up-to=1
+scripts/migrate version
