c1d3b7991f32739d0013293eda9f7ca8305d2e9b
Ahmed Shaheen <e.shaheen13@gmail.com>
2025-06-25 21:12:08 +0300

    fix tests regarding using .env file for configurations

--- tests/script-call-test.sh
+++ tests/script-call-test.sh
@@ -2,8 +2,35 @@
 
 set -e
 
-echo "depend on .env file variables"
+echo "MIGRATE_CONNECTION=sqlite:///tmp/teste.db
+MIGRATE_PATH=example/sqlite/" >> .env
+
+echo "Reset and update the database depending on .env file configurations"
+scripts/migrate reset --yes -vvv
 scripts/migrate version
+
+echo "Update the database to version 1 depending on .env file configurations"
+scripts/migrate update -vvv --up-to=1
+scripts/migrate version
+
+echo "Update the database to version 2 depending on .env file configurations"
+scripts/migrate up -vvv
+scripts/migrate version
+
+echo "Update the database to version 0 (base) depending on .env file configurations"
+scripts/migrate down -vvv --up-to=0
+scripts/migrate version
+
+echo "Update the database to version 0 (base) depending on .env file configurations"
+scripts/migrate up -vvv
+scripts/migrate down -vvv
+scripts/migrate version
+
+echo "Update the database to version 1 depending on .env file configurations"
+scripts/migrate update -vvv --up-to=1
+scripts/migrate version
+
+rm .env
 
 export MIGRATE_CONNECTION=sqlite:///tmp/teste.db
 export MIGRATE_PATH=example/sqlite/
